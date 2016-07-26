64ed2cc92e6ed872c110d91605d011c282ea3492
Dieuson VIRGILE <dvirgile@e2r8p16.42.fr>
2016-07-26 13:46:23 +0200

    add prompt

--- srcs/main.c
+++ srcs/main.c
@@ -6,28 +6,18 @@
 /*   By: dvirgile <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/27 11:40:17 by dvirgile          #+#    #+#             */
-/*   Updated: 2016/07/26 11:55:13 by sgaudin          ###   ########.fr       */
+/*   Updated: 2016/07/26 12:09:30 by dvirgile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int main(int argc, char **argv, char **envp)
+int			prompt(int argc, char **argv, char **envp)
 {
-	t_sh data;
-	char *line;
-	char **commands;
-	int ret;
-	pid_t pid;
-
-	ret = 0;
-	commands = NULL;
-	line = NULL;
-	pid = 0;
-	init_minishell(&data);
-	ft_putstr("$> ");
-	if (envp)
-		ft_putendl(envp[0]);
+	FT_INIT(char*, line, NULL);
+	FT_INIT(char**, commands, NULL);
+	FT_INIT(int, ret, 0);
+	FT_INIT(pid_t, pid, 0);
 	while (1)
 	{
 		ret = get_next_line(0, &line);
@@ -36,19 +26,30 @@ int main(int argc, char **argv, char **envp)
 			pid = fork();
 			if (pid == 0)
 			{
-				commands = lsh_read_line(line);
-				if (!ft_strcmp(commands[0], "cd"))
-					shell_cd("", commands);
-				else if (!ft_strcmp(commands[0], "ls"))
-					lsh_launch(commands);
-				free_simple_tab(&commands);
+			commands = lsh_read_line(line);
+			if (!ft_strcmp(commands[0], "cd"))
+				shell_cd("", commands);
+			else if (!ft_strcmp(commands[0], "ls"))
+				lsh_launch(commands);
+			free_simple_tab(&commands);
 			}
 			ret  = 0;
 			wait(&pid);
 			ft_putstr("\n$> ");
 		}
 	}
-	if (argc || argv)
+	if (argc || argv || envp)
 		return (0);
+	return (1);
+}
+
+int			main(int argc, char **argv, char **envp)
+{
+	t_sh	data;
+
+	init_minishell(&data, envp);
+	ft_putstr("$> ");
+	if (!prompt(argc, argv, envp))
+			exit(0);
 	return (0);
 }
