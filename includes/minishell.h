e30952a4364ac8d82a76ef52c44d1a8d11bfefe7
Simon GAUDIN <sgaudin@e2r8p15.42.fr>
2016-07-26 11:42:29 +0200

    done

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dvirgile <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/30 10:32:23 by dvirgile          #+#    #+#             */
-/*   Updated: 2016/06/30 10:32:32 by dvirgile         ###   ########.fr       */
+/*   Updated: 2016/07/26 11:41:49 by sgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 # define	MINISHELL_H
 # include <unistd.h>
 # include <stdio.h>
-# include <sys/types.h> 
+# include <sys/types.h>
 # include <sys/wait.h>
 # include "../srcs/libft/includes/libft.h"
 # include "../srcs/get_next_line/get_next_line.h"
+# ifndef FT_INIT
+	# define FT_INIT(t, n, v) t n = v
+# endif
 
-typedef struct 			s_sh_data
+typedef struct 			s_sh
 {
-	int 		(*fct_tab[3])(char *cmd, char **argv);
-}						t_sh_data;
+	char 				**env;
+}						t_sh;
 
 #endif
